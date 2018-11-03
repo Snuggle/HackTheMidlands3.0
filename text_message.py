816d23d63f9eac03d3ee1d407e203e1895feb604
Daniel <daniiel32@live.com>
2018-11-03 17:49:30 +0000

    Read keys from file!
    Co-authored-by: Snuggle <snuggle@sprinkly.net>x

--- text_message.py
+++ text_message.py
@@ -1,3 +1,16 @@
+import nexmo
+
+f = open("secrets.txt","r")
+lines = f.readlines()
+
+line = [x.strip() for x in lines]
+
+client = nexmo.Client(key=lines[0], secret=lines[1])
+
+
+
+
+
 client.send_message({
 'from': 'Acme Inc', 
 'to': TO_NUMBER,
