2daa5a1673c77356fe21e54e7e57a13c08d26bfc
Snuggle <snuggle@sprinkly.net>
2018-11-04 16:07:05 +0000

    Added code comments!
    Just in-case anyone is unsure what any of these lines of code do, or if they wish to use this code in the future!

--- text_message.py
+++ text_message.py
@@ -1,18 +1,21 @@
 #!/usr/bin/env python3
-
+# Import Nexmo-API library.
+# (If you get error, please run "pip3 install --user nexmo")
 import nexmo
 
+# Read file line-by-line.
 with open("secret.txt", "r") as f:
     lines = f.readlines()
-    line = [x.strip() for x in lines]
+    line = [x.strip() for x in lines] # This strips newline characters. (\n)
 
+# Create Nexmo client with our secret keys from file.
 client = nexmo.Client(key=lines[0], secret=lines[1])
 
+# 3rd line in file will be the phone number we will text.
 TO_NUMBER = lines[2]
 
-client.send_message({
-'from': 'Remind Me', 
-'to': TO_NUMBER,
-'text': 'testing 1234 is this working',
+# Create message as a Python dictionary.
+MessageDict = {'from': 'Remind Me', 'to': TO_NUMBER, 'text': 'testing 1234 is this working'}
 
-})
+# Send message!
+client.send_message(messageDict)
